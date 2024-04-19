279956c1e27b2a8fb156771aab7a94e4635925b9
Akash Kumar <akashku99at@gmail.com>
2024-04-19 13:46:21 +0530

    Gauge

---
+++ src/components/atoms/Gauge chart/GaugeChart.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+
+export default function GaugeDesign({ width, height, value }) {
+  return (
+    <Gauge
+      width={width}
+      height={height}
+      value={value}
+      cornerRadius="50%"
+      sx={(theme) => ({
+        [`& .${gaugeClasses.valueText}`]: {
+          fontSize: 40,
+        },
+        [`& .${gaugeClasses.valueArc}`]: {
+          fill: "#52b202",
+        },
+        [`& .${gaugeClasses.referenceArc}`]: {
+          fill: "#ECF5ED",
+        },
+      })}
+    />
+  );
+}
